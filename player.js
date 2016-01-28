8d401c8555f7be55ce1c150e0a69eeb6698f2f75
Gosstapher <gosstapher@gmail.com>
2016-01-28 12:04:10 +0000

    there is a player with a board position

---
+++ player.js
@@ -0,0 +1,6 @@
+var Player = function(name){
+  this.name = name;
+  this.position = 1;
+}
+
+module.exports = Player;
--- test.js
+++ test.js
@@ -1,10 +1,17 @@
 var assert = require('assert');
 var Board = require('./board')
+var Player = require('./player')
 
 describe('Game', function(){
   it('there should be a board with an array of 64 squares', function(){
     var board1 = new Board();
     board1.addSquares();
     assert.equal(64, board1.squares.length);
+  });
+  it('there should be a player that starts on square 1', function(){
+    var board1 = new Board();
+    board1.addSquares();
+    var player1 = new Player('Bob');
+    assert.equal(1, player1.position);
   });
 })
